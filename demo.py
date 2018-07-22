cb86c3d48bb4124823c036a65bb0cebcfe2095ff
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2018-07-21 21:58:43 -0600

    specify api to demo.py also add --help

--- demo.py
+++ demo.py
@@ -1,12 +1,30 @@
 #!/usr/bin/python
 
+"""
+%(scriptname)s [-h|--help] [api [api [...]]] [ticker [ticker [...]]]
+    no args: show some information about default tickers (%(defaultargs)s)
+    ticker(s): show some information about specified ticker(s)
+    api name(s) and ticker(s): show the specified api(s) result for the ticker(s)
+    yf and -h: show apis
+    api name(s) and -h: show help for that api
+    -h or --help: show usage
+"""
+
 from __future__ import print_function
+import sys
+import time
 from yahoofinancials import YahooFinancials as YF
 
-def doit(ticker):
-    mark = '-' * 64
+
+DEFAULT_ARGS = ('MMM', 'AAPL')
+MODULE_ARGS = ('yf', 'yahoofinancial', 'yahoofinancials')
+HELP_ARGS = ('-h', '--help')
+
+mark = '-' * 64
+
+
+def defaultapi(ticker):
     tick = YF(ticker)
-    print(mark)
     print(tick.get_stock_summary_data())
     print(mark)
     print(tick.get_stock_quote_type_data())
@@ -16,19 +34,54 @@ def doit(ticker):
     print(tick.get_current_price())
     print(mark)
     print(tick.get_dividend_rate())
-    print(mark)
     try:
-        print(tick._cache.keys())
-        print(mark)
+        r = tick._cache.keys()
     except AttributeError:
         pass
+    else:
+        print(mark)
+        print(r)
+
+
+def customapi(queries, ts):
+    yf = YF(ts[0] if 1 == len(ts) else ts)
+    for q in queries:
+        print('%s:' % (q,))
+        timeit(lambda: print(getattr(yf, q)()))
+
+
+def helpapi(queries):
+    if len(queries) == 1:
+        print(__doc__ % {'scriptname': sys.argv[0], 'defaultargs': ', '.join(DEFAULT_ARGS)})
+    else:
+        import pydoc
+        for q in queries:
+            if q in MODULE_ARGS:
+                print(pydoc.render_doc(YF, "Help on %s"))
+            elif q not in HELP_ARGS:
+                print(pydoc.render_doc(getattr(YF, q), "Help on %s"))
+
+
+def timeit(f, *args):
+    print(mark)
+    st = time.time()
+    f(*args)
+    et = time.time()
+    print(mark)
+    print(et - st, 'seconds')
 
 
 
 if __name__ == '__main__':
-    import sys
-    import time
-    ts = sys.argv[1:] or ['MMM', 'AAPL']
-    st = time.time()
-    doit(ts[0] if 1 == len(ts) else ts)
-    print(time.time() - st, 'seconds')
+    api = set(s for s in dir(YF) if s.startswith('get_'))
+    api.update(MODULE_ARGS)
+    api.update(HELP_ARGS)
+    ts = sys.argv[1:]
+    queries = [q for q in ts if q in api]
+    ts = [t for t in ts if not t in queries] or DEFAULT_ARGS
+    if [h for h in HELP_ARGS if h in queries]:
+        helpapi(queries)
+    elif queries:
+        customapi(queries, ts)
+    else:
+        timeit(defaultapi, ts[0] if 1 == len(ts) else ts)
