3df7243a44ce2f148b1cb1a3c5135acd3b0b3525
Sylvan Butler <gitstuff.sylvan@xoxy.net>
2020-03-06 19:55:52 -0700

    yahoo issue was the client version

--- yahoofinancials/__init__.py
+++ yahoofinancials/__init__.py
@@ -68,7 +68,7 @@ class ManagedException(Exception):
 
 # Class used to open urls for financial data
 class UrlOpener(FancyURLopener):
-    version = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; it; rv:1.8.1.11) Gecko/20071127 Firefox/2.0.0.11'
+    version = 'w3m/0.5.3+git20180125'
 
 
 # Class containing Yahoo Finance ETL Functionality
