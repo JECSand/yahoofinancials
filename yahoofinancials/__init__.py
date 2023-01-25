3685ea7c240ef7a820ff6182efe178d0a64f3054
Connor Sanders <jecsand@pm.me>
2023-01-25 12:37:55 -0600

    fix for new decryption issue

--- yahoofinancials/__init__.py
+++ yahoofinancials/__init__.py
@@ -1,9 +1,1 @@
-from multiprocessing import freeze_support, set_start_method
-
-
-if __name__ == '__main__':
-    freeze_support()
-    set_start_method("spawn")
-    from yahoofinancials.yf import YahooFinancials
-else:
-    from yahoofinancials.yf import YahooFinancials
+from yahoofinancials.yf import YahooFinancials
